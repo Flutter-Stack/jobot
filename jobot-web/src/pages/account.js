f315c84379e0cb82e4913b645e8ea2dbe5ee20fb
suresh-kumara-gist <kumara.suresh25@gmail.com>
2023-04-25 12:54:01 +0200

    added debug messages

--- jobot-web/src/pages/account.js
+++ jobot-web/src/pages/account.js
@@ -52,11 +52,11 @@ export default function AccountPage() {
     updateUserProfile(supabase, profileData);
   }
 
- console.log("profile Data");
- console.log(profileData);
-  // if (!profileData) {
-  //   return null;
-  // }
+//  console.log("profile Data");
+//  console.log(profileData);
+  if (!profileData) {
+    return null;
+  }
 
   return (
     <>
