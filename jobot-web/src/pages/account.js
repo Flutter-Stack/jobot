8606d8af1ea3900b94c32b4275bd333d3eb28eba
suresh-kumara-gist <kumara.suresh25@gmail.com>
2023-04-25 12:17:43 +0200

    added debug messages

--- jobot-web/src/pages/account.js
+++ jobot-web/src/pages/account.js
@@ -8,7 +8,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-// import { toast } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 async function updateUserProfile(supabase, profileData) {
   try {
