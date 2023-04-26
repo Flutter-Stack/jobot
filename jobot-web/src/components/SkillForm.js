235432dcabce903d08de97f11d883fb89719528b
suresh-kumara-gist <kumara.suresh25@gmail.com>
2023-04-26 11:32:52 +0200

    skill form

--- jobot-web/src/components/SkillForm.js
+++ jobot-web/src/components/SkillForm.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import SkillInput from "./SkillInput";
-import { makeDisplayName } from "@/utils";
+// import { useState } from "react";
+// import SkillInput from "./SkillInput";
+// import { makeDisplayName } from "@/utils";
 import { useUser } from "@supabase/auth-helpers-react";
-import Link from "next/link";
+// import Link from "next/link";
 
 export function fillTemplate(string, data = {}) {
   return Object.entries(data).reduce((res, [key, value]) => {
@@ -23,69 +23,26 @@ const SkillForm = ({ skill, sendMessages }) => {
   console.log(sendMessages);
 
   const user = useUser();
-  const [inputData, setInputData] = useState({});
+  // const [inputData, setInputData] = useState({});
 
   const inputs = skill.inputs || [];
   console.log("inputs");
   console.log(inputs);
-  function startConversation() {
-    const filledMessages = [
-      { role: "system", content: fillTemplate(skill.system_prompt, inputData) },
-      { role: "user", content: fillTemplate(skill.user_prompt, inputData) },
-    ];
+  // function startConversation() {
+  //   const filledMessages = [
+  //     { role: "system", content: fillTemplate(skill.system_prompt, inputData) },
+  //     { role: "user", content: fillTemplate(skill.user_prompt, inputData) },
+  //   ];
 
-    sendMessages(filledMessages);
-  }
+  //   sendMessages(filledMessages);
+  // }
 
   if (!skill) {
     return <div>Not Found</div>;
   }
 
   return (
-    <div className="mx-auto my-4 w-full max-w-4xl px-2">
-      <h1 className="text-center mx-auto text-4xl font-medium">
-        {skill?.title}
-      </h1>
-      <div className="mx-auto mt-4 mb-4 max-w-xl text-center text-gray-500 sm:text-base">
-        {skill?.description}
-      </div>
-      <div>
-        {inputs?.map((inputInfo) => (
-          <SkillInput
-            key={inputInfo.field}
-            {...inputInfo}
-            value={inputData[inputInfo.field]}
-            onChange={(value) =>
-              setInputData({ ...inputData, [inputInfo.field]: value })
-            }
-          />
-        ))}
-      </div>
-
-      <div className="flex justify-between">
-        <button
-          type="button"
-          onClick={startConversation}
-          className="rounded-md  bg-blue-500 py-2 px-3 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-blue-600 active:bg-blue-700 dark:ring-0"
-        >
-          Start Conversation
-        </button>
-
-        {skill?.user_id !== user?.id ? (
-          <div className="text-gray-500 font-medium text-sm">
-            Author: {makeDisplayName(skill?.user_id)}
-          </div>
-        ) : (
-          <Link
-            href={`/${skill.user_id.username}/${skill.slug}/edit`}
-            type="submit"
-            className="ml-3 rounded-md bg-white py-2 px-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 active:bg-gray-100"
-          >
-            Edit Skill
-          </Link>
-        )}
-      </div>
-    </div>
+    <div>asdfafadf</div>
   );
 };
 
